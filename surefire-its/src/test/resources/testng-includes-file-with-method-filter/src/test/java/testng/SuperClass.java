99e5a36fe4ff2633cad603fefa82671cfe89c60b
Philipp Dolif <git.phil.cd@gmail.com>
2025-11-24 19:15:13 +0100

    Add IT for using includesFile with method filter with TestNG

---
+++ surefire-its/src/test/resources/testng-includes-file-with-method-filter/src/test/java/testng/SuperClass.java
@@ -0,0 +1,12 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class SuperClass {
+
+    @Test
+    public void testInSuperClass() {
+        System.out.println("TestNG: SuperClass#testInSuperClass executed");
+    }
+
+}
